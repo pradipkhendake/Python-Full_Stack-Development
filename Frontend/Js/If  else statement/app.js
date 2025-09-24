4020219eb421f6f1bba589aecc709619211cbe1b
Pradip Khendake <152958973+pradipkhendake@users.noreply.github.com>
2025-09-24 12:31:40 +0530

    .

--- Frontend/Js/If  else statement/app.js
+++ Frontend/Js/If  else statement/app.js
@@ -2,6 +2,6 @@ let a=33
 if(a>=18){
     console.log('You are eligible to vote');
 }
-if(a<18){
+else if(a<18){
      console.log('You are not eligible to vote');
 }
---
+++ Frontend/Js/else statement/app.js
@@ -0,0 +1,13 @@
+let size="M" ;
+if(size=="XL"){
+    console.log("Price is RS.250")
+}
+else if(size=="L"){
+    console.log("Price is RS.200")
+}
+else if(size=="M"){
+    console.log("Price is RS.100")
+}
+else if(size=="S"){
+    console.log("Price is RS.50")
+}
