05a59fb34176547c7db15d6638fb5f45a56b2863
Pradip Khendake <152958973+pradipkhendake@users.noreply.github.com>
2025-09-25 10:53:08 +0530

    .

---
+++ Frontend/Js/Part 3/ps3/app.js
@@ -0,0 +1,7 @@
+let lang=['c','c++','html','javascript','python','java','c#','sql']
+console.log(lang);
+let oppo = lang.reverse();
+console.log(oppo);
+let index = oppo.indexOf("javascript");
+console.log(index)
+
