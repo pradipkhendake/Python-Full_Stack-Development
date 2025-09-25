bbe6891b8830e4ddccf1f64c81ef508065219cc9
Pradip Khendake <152958973+pradipkhendake@users.noreply.github.com>
2025-09-25 10:31:36 +0530

    25/9/25

---
+++ Frontend/Js/Part 3/ps2/app.js
@@ -0,0 +1,8 @@
+let names = "ApnaCollege";
+let sliced_names = names.slice(4,names.length)
+console.log(names)
+console.log(sliced_names)
+
+let replaced = sliced_names.replace('l','t').replace('l','t')
+console.log(replaced)
+
